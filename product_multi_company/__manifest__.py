248634b9fd60e7c46e3ae681cc417f24881e9900
Dave Lasley <dave@laslabs.com>
2016-12-26 12:13:33 -0800

    [MIG] product_multi_company: Upgrade to v10 * Rename manifest * Change openerp references to odoo * Bump version

--- product_multi_company/__manifest__.py
+++ product_multi_company/__manifest__.py
@@ -9,7 +9,7 @@
               "Odoo Community Association (OCA)",
     'website': "https://www.tecnativa.com",
     'category': 'Product Management',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'product',
--- product_multi_company/hooks.py
+++ product_multi_company/hooks.py
@@ -2,7 +2,7 @@
 # Copyright 2015-2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import api, SUPERUSER_ID
+from odoo import api, SUPERUSER_ID
 
 
 def post_init_hook(cr, registry):
