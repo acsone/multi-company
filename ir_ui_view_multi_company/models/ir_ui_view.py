ee635d3334626be08ba2fb8ba8777aa51ba59712
sbejaoui <souheil.bejaoui@acsone.eu>
2025-04-09 16:43:11 +0200

    [ADD] ir_ui_view_multi_company

---
+++ ir_ui_view_multi_company/models/ir_ui_view.py
@@ -0,0 +1,26 @@
+# Copyright 2025 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+from odoo.osv.expression import AND
+
+
+class IrUiView(models.Model):
+    _inherit = "ir.ui.view"
+
+    company_id = fields.Many2one("res.company", string="Company", index=True)
+
+    @api.model
+    def _get_inheriting_views_domain(self):
+        domain = super()._get_inheriting_views_domain()
+        domain = domain if domain else []
+        return AND(
+            [
+                domain,
+                [
+                    "|",
+                    ("company_id", "=", False),
+                    ("company_id", "=", self.env.company.id),
+                ],
+            ]
+        )
